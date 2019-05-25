3f73d29a383b4222cc712120020dc73ddd8ab6a8
Joshua Kidd <kidd.josh.343@gmail.com>
2019-05-25 11:59:28 -0500

    Updated some things. Added flutter icons for launcher icons

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -22,56 +22,120 @@ class ResultsPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF111111),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Center(
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32.0,
+                vertical: 18.0,
+              ),
               child: Text(decision.objective, style: _headerStyle),
             ),
           ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Pros (${decision.pros.length})",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  for (var p in decision.pros)
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("PROS SCORE", style: TextStyle(color: Colors.grey)),
+                    SizedBox(height: 8.0),
                     Text(
-                      p.title,
-                      style: TextStyle(color: Colors.white),
+                      "${proScore.toInt()}",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20.0,
+                      ),
                     ),
-                ],
-              ),
-              SizedBox(width: 16.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Cons (${decision.cons.length})",
-                    style: TextStyle(color: Colors.white),
+                  ],
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      modScore.toInt().toString(),
+                      style: TextStyle(
+                        fontSize: 45.0,
+                        color: (modScore > 0) ? Colors.green : Colors.red,
+                      ),
+                    ),
                   ),
-                  for (var c in decision.cons)
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("CONS SCORE", style: TextStyle(color: Colors.grey)),
+                    SizedBox(height: 8.0),
                     Text(
-                      c.title,
-                      style: TextStyle(color: Colors.white),
+                      "${conScore.toInt()}",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20.0,
+                      ),
                     ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-          Text("Mood: ${decision.mood}"),
-          Text("Pro score: $proScore"),
-          Text("Con score: $conScore"),
-          Text("Mod score: $modScore"),
-          for (var d in decision.pros) Text(d.title),
-          for (var d in decision.cons) Text(d.title),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 32.0,
+              vertical: 20.0,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "PROS (${decision.pros.length})",
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    for (var p in decision.pros)
+                      Text(
+                        p.title,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    SizedBox(height: 8.0),
+                  ],
+                ),
+                Expanded(child: Container()),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "CONS (${decision.cons.length})",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 22.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    for (var c in decision.cons)
+                      Text(
+                        c.title,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    SizedBox(height: 8.0),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
